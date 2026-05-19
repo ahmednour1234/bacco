5d21e3bce9f89187f29e5960ebdada83e7af90b5
Ahmed Nour <106186787+ahmednour1234@users.noreply.github.com>
2026-05-19 18:30:59 +0300

    dsf

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,5 +1,5 @@
 <img {{ $attributes->merge(['class' => '']) }}
-     src="{{ asset('images/logo1.png') }}"
+     src="/public/images/logo1.png"
      width="320" height="90"
      alt="Qimta – Construction Pricing Platform Saudi Arabia"
      fetchpriority="high"
