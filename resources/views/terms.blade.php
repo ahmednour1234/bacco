f6332a4206d956c3c3f907511d5ccf6b94d04e4e
Ahmed Nour <106186787+ahmednour1234@users.noreply.github.com>
2026-05-19 18:35:49 +0300

    شسي

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -394,7 +394,7 @@
                 </div>
                 {{-- Circuit board tech image --}}
                 <img
-                    src="{{ asset('images/circuit-board.svg') }}"
+                    src="/public/images/circuit-board.svg"
                     alt="Security Technology"
                     class="terms-circuit-img"
                     loading="lazy"
