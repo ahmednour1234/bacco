d63f47eeb9b20a5499eaaec9daf9ae2fc6f8fe14
Ahmed Nour <106186787+ahmednour1234@users.noreply.github.com>
2026-05-24 14:13:45 +0300

    Revert "s"
    This reverts commit 19db8ac7c3a2ad8d2e8a034f1c86b39acba796a5.

--- resources/views/livewire/enduser/quotations/partials/boq-upload-and-items.blade.php
+++
@@ -1,4 +0,0 @@
-{{-- Placeholder for BOQ upload and items section --}}
-<div class="alert alert-info">
-    This is a placeholder for the BOQ upload and items section. Please implement the required functionality here.
-</div>
