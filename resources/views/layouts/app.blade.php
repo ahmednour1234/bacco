a3cb39fad4ac02b2c89866cf7236b66d52fe0fc5
ahmednour1234 <ahmednour5999@gmail.com>
2026-06-24 18:01:48 +0300

    das

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', $__defaultTitle)</title>
+    @include('partials.favicon')
     <meta name="description" content="@yield('description', $__defaultDesc)">
     {{-- Canonical + Hreflang: /ar/ prefix routes for Google-crawlable Arabic --}}
     @php
---
+++ resources/views/partials/favicon.blade.php
@@ -0,0 +1,5 @@
+{{-- Qimta favicon (browser tab icon). SVG for modern browsers, ICO/PNG fallbacks. --}}
+<link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+<link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon.svg') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon-32.png') }}">
+<link rel="apple-touch-icon" href="{{ asset('images/apple-touch-icon.png') }}">
