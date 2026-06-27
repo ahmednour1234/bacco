bc653e1578ccbfd04b4eeec5f5fe3371d940b3de
ahmednour1234 <ahmednour5999@gmail.com>
2026-06-27 18:10:47 +0300

    das

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,5 +1,5 @@
 <img {{ $attributes->merge(['class' => '']) }}
-     src="{{ asset('images/logo1.png') }}?v=2"
+     src="{{ asset('images/logo1.png?v=2') }}"
      width="320" height="90"
      alt="Qimta – Construction Pricing Platform Saudi Arabia"
      fetchpriority="high"
